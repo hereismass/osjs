d8bbcd1062ed529db34d2a03897e2226da1e9a0f
Massimo Zambelli <mass.zambelli@gmail.com>
2015-04-02 19:03:24 +0200

    added screen/import/alias/group and timestamp on each event

--- osjs.js
+++ osjs.js
@@ -1,7 +1,9 @@
 //xhr object
 function xhr(url, write_key, data){
 	//we add timestamp to data
-	//todo
+	var d = new Date();
+	var t = d.toISOString();
+	data.timestamp = t;
 	var xmlhttp;
 	if (window.XMLHttpRequest){
 		// code for IE7+, Firefox, Chrome, Opera, Safari
@@ -35,6 +37,7 @@ function xhr(url, write_key, data){
 //osjs main class
 function Osjs(){
 	this.write_key = null;
+	this.datas = [];
 	var self = this;
 
 	//options
@@ -44,17 +47,22 @@ function Osjs(){
 		}
 	}
 
-	this.queue = function(){
-
+	this.queue = function(url, data){
+		//todo
 	}
 
 	this.sendQueue = function(){
-
+		//todo
 	}
 
 	//identify
 	this.identify = function(data){
 		xhr("https://api.segment.io/v1/identify", self.write_key, data);
+	}
+
+	//group
+	this.group = function(data){
+		xhr("https://api.segment.io/v1/group", self.write_key, data);
 	}
 
 	//track
@@ -65,6 +73,21 @@ function Osjs(){
 	//page
 	this.page = function(data){
 		xhr("https://api.segment.io/v1/page", self.write_key, data);
+	}
+
+	//screen
+	this.screen = function(data){
+		xhr("https://api.segment.io/v1/screen", self.write_key, data);
+	}
+
+	//alias
+	this.alias = function(data){
+		xhr("https://api.segment.io/v1/alias", self.write_key, data);
+	}
+
+	//import
+	this.import = function(data){
+		xhr("https://api.segment.io/v1/import", self.write_key, data);
 	}
 }
 
