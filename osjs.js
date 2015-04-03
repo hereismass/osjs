fd04fbcb936b52fd7c25c0a91bd5f4bbeb05a5ee
Massimo Zambelli <mass.zambelli@gmail.com>
2015-04-03 11:25:10 +0200

    refactoring of Osjs class
    xhr function now a method of the osjs object

--- osjs.js
+++ osjs.js
@@ -1,39 +1,3 @@
-//xhr object
-function xhr(url, write_key, data){
-	//we add timestamp to data
-	var d = new Date();
-	var t = d.toISOString();
-	data.timestamp = t;
-	var xmlhttp;
-	if (window.XMLHttpRequest){
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-	    xmlhttp=new XMLHttpRequest();
-	}
-	else{
-		// code for IE6, IE5
-	    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	}
-	xmlhttp.onreadystatechange = function(){
-		if(xmlhttp.readyState==4){
-			if(xmlhttp.status == 200){
-				//success, we can send the queue if there is one
-				osjs.sendQueue();
-			}
-			else{
-				//error => queue
-				osjs.queue(url, data);
-			}
-			//response todo
-			console.log("xhr resp : " + xmlhttp.responseText + ' ' + xmlhttp.status);
-		}
-	}
-	xmlhttp.open("POST", url, true);
-	xmlhttp.setRequestHeader("Authorization", "Basic " + btoa(write_key + ":" + null));
-	xmlhttp.setRequestHeader("Content-type","application/json");
-	//xmlhttp.setRequestHeader("Content-length", data.length);
-	xmlhttp.send(JSON.stringify(data));
-}
-
 //osjs main class
 function Osjs(){
 	this.write_key = null;
@@ -47,6 +11,42 @@ function Osjs(){
 		}
 	}
 
+	//xhr func
+	this.xhr = function(url, data){
+		//we add timestamp to data
+		var d = new Date();
+		var t = d.toISOString(); //segment need timestamp at ISO format
+		data.timestamp = t;
+		var xmlhttp;
+		if (window.XMLHttpRequest){
+			// code for IE7+, Firefox, Chrome, Opera, Safari
+		    xmlhttp=new XMLHttpRequest();
+		}
+		else{
+			// code for IE6, IE5
+		    xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+		xmlhttp.onreadystatechange = function(){
+			if(xmlhttp.readyState==4){
+				if(xmlhttp.status == 200){
+					//success, we can send the queue if there is one
+					osjs.sendQueue();
+				}
+				else{
+					//error => queue
+					osjs.queue(url, data);
+				}
+				//response todo
+				console.log("xhr resp : " + xmlhttp.responseText + ' ' + xmlhttp.status);
+			}
+		}
+		xmlhttp.open("POST", url, true);
+		xmlhttp.setRequestHeader("Authorization", "Basic " + btoa(self.write_key + ":" + null));
+		xmlhttp.setRequestHeader("Content-type","application/json");
+		//xmlhttp.setRequestHeader("Content-length", data.length);
+		xmlhttp.send(JSON.stringify(data));
+	}
+
 	this.queue = function(url, data){
 		//todo
 	}
@@ -57,37 +57,37 @@ function Osjs(){
 
 	//identify
 	this.identify = function(data){
-		xhr("https://api.segment.io/v1/identify", self.write_key, data);
+		xhr("https://api.segment.io/v1/identify", data);
 	}
 
 	//group
 	this.group = function(data){
-		xhr("https://api.segment.io/v1/group", self.write_key, data);
+		xhr("https://api.segment.io/v1/group", data);
 	}
 
 	//track
 	this.track = function(data){
-		xhr("https://api.segment.io/v1/track", self.write_key,data);
+		xhr("https://api.segment.io/v1/track", data);
 	}
 
 	//page
 	this.page = function(data){
-		xhr("https://api.segment.io/v1/page", self.write_key, data);
+		xhr("https://api.segment.io/v1/page", data);
 	}
 
 	//screen
 	this.screen = function(data){
-		xhr("https://api.segment.io/v1/screen", self.write_key, data);
+		xhr("https://api.segment.io/v1/screen", data);
 	}
 
 	//alias
 	this.alias = function(data){
-		xhr("https://api.segment.io/v1/alias", self.write_key, data);
+		xhr("https://api.segment.io/v1/alias", data);
 	}
 
 	//import
 	this.import = function(data){
-		xhr("https://api.segment.io/v1/import", self.write_key, data);
+		xhr("https://api.segment.io/v1/import", data);
 	}
 }
 
